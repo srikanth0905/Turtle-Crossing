b54e972a73b1392351f7ea81a9bf0ff957b64ef0
Greymat9er <greymat9er@gmail.com>
2023-03-21 19:31:48 +0530

    create level when finished up on starting line and increase car speed

--- car_manager.py
+++ car_manager.py
@@ -10,6 +10,7 @@ class CarManager:
 
     def __init__(self):
         self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_car(self):
         # this loop is only to slow down creation of cars, otherwise cars were getting created every 0.1 secs
@@ -25,4 +26,7 @@ class CarManager:
 
     def move_cars(self):
         for car in self.all_cars:
-            car.backward(STARTING_MOVE_DISTANCE)
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
