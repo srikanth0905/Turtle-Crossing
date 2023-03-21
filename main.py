e0d16b53c0c4d740cd1a2bcca0a017b74544ef21
Greymat9er <greymat9er@gmail.com>
2023-03-21 19:20:17 +0530

    create cars and move across screen

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+
 screen.listen()
 screen.onkeypress(player.go_up, "Up")
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
